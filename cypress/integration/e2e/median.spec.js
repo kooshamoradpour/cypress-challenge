292d2992a27e83d55e43a8c5c2e8503844affeaf
koosha <kushamoradpour@gmail.com>
2021-09-26 17:56:53 -0400

    add test cases for input types

--- cypress/integration/e2e/median.spec.js
+++ cypress/integration/e2e/median.spec.js
@@ -4,4 +4,22 @@ describe("Median App", () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
     })
+
+    it('Should Display Header', () => {
+        cy.get('h1').contains('Enter a number to get the median of primes:')
+    })
+
+    it('Should not accept string input', () => {
+        cy.get('input')
+        .type('bad-input')
+        .should('not.have.value', 'bad-input')
+    })
+
+    it('Should accept number inputs', () => {
+        cy.get('input')
+        .should('have.attr', 'type', 'number')
+        .type(10)
+        .should('have.value', 10)
+    })
+
 })
